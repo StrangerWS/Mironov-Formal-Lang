137d957b74cba94dade6a22659ef2e4592b83aa2
StrangerWS <strangernf@gmail.com>
2017-09-26 12:25:27 +0400

    Patch

--- src/com/ssu/strangerws/formallang/automation/Automation.java
+++ src/com/ssu/strangerws/formallang/automation/Automation.java
@@ -3,10 +3,7 @@ package com.ssu.strangerws.formallang.automation;
 import javafx.util.Pair;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by DobryninAM on 05.09.2017.
@@ -15,6 +12,11 @@ public class Automation {
     private Set<String> startStates;
     private Set<String> endStates;
     private Map<String, Pair<String, String>> transitions;
+    private String state;
+
+    public String getState() {
+        return state;
+    }
 
     public Set<String> getStartStates() {
         return startStates;
@@ -80,7 +82,7 @@ public class Automation {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] arr = line.split(" ");
-                Pair tmp = new Pair(arr[1], arr[2]);
+                Pair<String, String> tmp = new Pair(arr[1], arr[2]);
                 transitions.put(arr[0], tmp);
             }
         } catch (FileNotFoundException e) {
@@ -92,21 +94,43 @@ public class Automation {
         return transitions;
     }
 
-    private boolean checkInputValidity(List<String> input){
-        boolean flag = true;
+    public void getStatesFromData(String fileName){
+        Set<String> startStates = new HashSet<String>();
+        Set<String> endStates = new HashSet<String>();
 
-        for (String s : startStates) {
-            for (Map.Entry entry : transitions.entrySet()) {
-                if (!entry.getKey().equals(s)) {
-                    flag = false;
-                    System.err.println("INVALID INPUT");
-                    break;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if(line.contains(">")){
+                    startStates.add(line.substring(1));
+                }
+                if(line.contains("\\")){
+                    endStates.add(line.substring(1));
                 }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return flag;
     }
 
-    
+    public boolean execute(String expression){
+        String[] array = expression.split(" ");
+        for (int i = 0; i < array.length; i++) {
+            if(stateChange(array[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean stateChange(String cmd){
+        Pair<String, String> transition = transitions.get(cmd);
+        if (state.equals(transition.getKey()) && endStates.contains(transition.getValue())){
+            state = transition.getValue();
+            return true;
+        } else return false;
+    }
 }
