29d2952962d673d3c57752dfad3ec885814a36a7
StrangerWS <strangernf@gmail.com>
2017-10-31 15:57:55 +0300

    #1 DFA: DONE NFA: DONE #2 FUNC: Ready REAL: Not ready #3: LEX: Not ready #4: Not ready #5: Not ready

--- src/main/java/com/ssu/strangerws/formallang/Main.java
+++ src/main/java/com/ssu/strangerws/formallang/Main.java
@@ -11,12 +11,15 @@ public class Main {
     public static void main(String[] args) {
         Automation automation = new DFA("src\\main\\resources\\txt\\automation.txt");
         Automation automation1 = new NFA("src\\main\\resources\\txt\\nfa.txt");
+        Automation automation2 = new DFA("src\\main\\resources\\txt\\real.txt");
         String sentence = "be";
         String sentence1 = "vggvvggvg";
+        String sentence2 = "-13.3e";
 
         try {
             System.out.println(AutomationUtils.testAutomation(automation, sentence));
             System.out.println(AutomationUtils.testAutomation(automation1, sentence1));
+            System.out.println(AutomationUtils.testAutomation(automation2, sentence2));
 
         } catch (IOException e) {
             e.printStackTrace();
