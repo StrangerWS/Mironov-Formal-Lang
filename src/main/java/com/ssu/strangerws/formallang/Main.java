f69e3d4ea685754c3a8faf1ce0e9dc35f2bdda4a
StrangerWS <strangernf@gmail.com>
2017-11-07 15:51:59 +0300

    #1: DONE #2: DONE #3: Not ready #4: Not ready #5: Not ready

--- src/main/java/com/ssu/strangerws/formallang/Main.java
+++ src/main/java/com/ssu/strangerws/formallang/Main.java
@@ -9,16 +9,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-//        Automation automation = new DFA("src\\main\\resources\\txt\\dfa.txt");
-//        Automation automation1 = new NFA("src\\main\\resources\\txt\\nfa.txt");
+//      Automation automation = new DFA("src\\main\\resources\\txt\\dfa.txt");
+//      Automation automation1 = new NFA("src\\main\\resources\\txt\\nfa.txt");
         Automation automation2 = new DFA("src\\main\\resources\\txt\\real.txt");
-//        String sentence = "be";
-//        String sentence1 = "vggvvggvg";
-        String sentence2 = "-13.3ee,rlltp+5.3gaf0.e.043e+12";
+//      String sentence = "be";
+//      String sentence1 = "vggvvggvg";
+        String sentence2 = "-13.3e-7e,rl.ltp+5.3egaf0.dsere.043e+12";
 
         try {
-//            System.out.println(AutomationUtils.testAutomation(automation, sentence));
-//            System.out.println(AutomationUtils.testAutomation(automation1, sentence1));
+//          System.out.println(AutomationUtils.testAutomation(automation, sentence));
+//          System.out.println(AutomationUtils.testAutomation(automation1, sentence1));
             System.out.println(AutomationUtils.findAllExpressions(automation2, sentence2));
         } catch (IOException e) {
             e.printStackTrace();
