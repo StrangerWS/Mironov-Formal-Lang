88e855784784afd33177c3a3d69a0795de361d2f
StrangerWS <strangernf@gmail.com>
2017-11-21 23:57:47 +0400

    1. Ready 2. Ready 3. Done (not checked) 4. Not ready 5. Not ready

--- src/main/java/com/ssu/strangerws/formallang/Main.java
+++ src/main/java/com/ssu/strangerws/formallang/Main.java
@@ -11,24 +11,17 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
-        Automation identifier = new DFA("src\\main\\resources\\txt\\identifier.txt", 1, Type.identifier);
-        Automation real = new DFA("src\\main\\resources\\txt\\real.txt", 2, Type.real);
-        Automation integer = new DFA("src\\main\\resources\\txt\\int.txt", 3, Type.integer);
-        Automation closeBracket = new DFA("src\\main\\resources\\txt\\closeBkt.txt", 4, Type.closeBracket);
-        Automation openBracket = new DFA("src\\main\\resources\\txt\\openBkt.txt", 5, Type.openBracket);
-        Automation space = new DFA("src\\main\\resources\\txt\\space.txt", 6, Type.space);
-        Automation keyWord = new DFA("src\\main\\resources\\txt\\keyWord.txt", 7, Type.keyWord);
+        Automation identifier =     new DFA("src\\main\\resources\\txt\\identifier.txt",    1, Type.identifier);
+        Automation real =           new DFA("src\\main\\resources\\txt\\real.txt",          2, Type.real);
+        Automation integer =        new DFA("src\\main\\resources\\txt\\int.txt",           3, Type.integer);
+        Automation closeBracket =   new DFA("src\\main\\resources\\txt\\closeBkt.txt",      4, Type.closeBracket);
+        Automation openBracket =    new DFA("src\\main\\resources\\txt\\openBkt.txt",       5, Type.openBracket);
+        Automation space =          new DFA("src\\main\\resources\\txt\\space.txt",         6, Type.space);
+        Automation keyWord =        new DFA("src\\main\\resources\\txt\\keyWord.txt",       7, Type.keyWord);
 
         try {
-            String sentence2 = AutomationUtils.readFile("src\\main\\resources\\code.txt");
-            System.out.println(AutomationUtils.analyzeCode(new Automation[]{identifier,
-                                                                            real,
-                                                                            integer,
-                                                                            closeBracket,
-                                                                            openBracket,
-                                                                            space,
-                                                                            keyWord},
-                                                                            sentence2));
+            System.out.println(AutomationUtils.analyzeCode(new Automation[]{identifier, real, integer, closeBracket, openBracket, space, keyWord},
+                                                                            AutomationUtils.readFile("src\\main\\resources\\code.txt")));
         } catch (IOException e) {
             e.printStackTrace();
         }
